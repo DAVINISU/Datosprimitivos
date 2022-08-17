2f298f1354b12036e2370acfeff67d2e94a4c9ce
505 <505@academicos.cesde.edu.co>
2022-08-17 17:34:30 -0500

    Se crean datos primitivos

---
+++ main.py
@@ -0,0 +1,23 @@
+# Hola me llamo comentario de línea
+
+''' 
+Hola soy comentario de bloque
+'''
+# Quemar valores en una variable
+# Datos primitivos
+nombre = 'David'
+edad = 27
+estatura =1.83
+hinchadelmejor = True # Siempre con mayuscula.
+
+# las variables son costosas para la memoria, 
+# se debe usar la menor cantidad de variables posibles.
+
+# Datos de Salidas
+print("Hola;",nombre)
+print(f'hola {nombre} tu estatura es: {estatura}')
+
+
+
+
+
